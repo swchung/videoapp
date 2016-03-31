c34ef3097b7ebe4a9c6a44aea45c651acc3e148c
Steven Chung <steven.chung@appian.com>
2016-03-31 11:27:19 -0400

    Initial Commit

---
+++ VideoAppTests/VideoAppTests.m
@@ -0,0 +1,39 @@
+//
+//  VideoAppTests.m
+//  VideoAppTests
+//
+//  Created by Steven Chung on 3/31/16.
+//  Copyright © 2016 Steven Chung. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VideoAppTests : XCTestCase
+
+@end
+
+@implementation VideoAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
